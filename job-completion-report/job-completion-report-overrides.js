8025c0a5160451137efbab5c04388aa6ef6ddb04
Julian Birch <103764638+julianbirch0@users.noreply.github.com>
2026-06-23 14:52:03 +0100

    Add job completion photo section

--- job-completion-report/job-completion-report-overrides.js
+++ job-completion-report/job-completion-report-overrides.js
@@ -1,3 +1,18 @@
+var customerPhotos = [];
+var beforePhotos = [];
+var afterPhotos = [];
+var originalOnload = window.onload;
+
+window.onload = function () {
+  if (originalOnload) {
+    originalOnload();
+  }
+
+  setupPhotoUploadControl("customerPhotoFiles", customerPhotos);
+  setupPhotoUploadControl("beforePhotoFiles", beforePhotos);
+  setupPhotoUploadControl("afterPhotoFiles", afterPhotos);
+};
+
 sampleJson.document.title = "Job Completion Report";
 sampleJson.work_order_number = "12345";
 
@@ -22,6 +37,7 @@ function renderBody(data) {
       renderWorkOrderSummary(data) +
       renderServiceSection(data) +
       renderReceivingFacilitySection(data) +
+      renderPhotosSection() +
     '</main>';
 }
 
@@ -43,4 +59,90 @@ function renderServiceSection(data) {
       '<div class="site-address">SITE ADDRESS:&nbsp; ' + escapeHtml(getValue(data, "site_name")) + ' &nbsp;' + escapeHtml(getValue(data, "site_address")) + '</div>' +
       renderWasteItemsTable(data) +
     '</section>';
+}
+
+function setupPhotoUploadControl(inputId, targetPhotos) {
+  var input = document.getElementById(inputId);
+
+  if (!input) {
+    return;
+  }
+
+  input.onchange = function (event) {
+    readPhotoFiles(event.target.files, targetPhotos, function () {
+      event.target.value = "";
+      renderFromJsonInput();
+    });
+  };
+}
+
+function readPhotoFiles(files, targetPhotos, callback) {
+  var index = 0;
+
+  if (!files || !files.length) {
+    callback();
+    return;
+  }
+
+  function readNextPhoto() {
+    var file;
+    var reader;
+
+    if (index >= files.length) {
+      callback();
+      return;
+    }
+
+    file = files[index];
+    index += 1;
+
+    if (!file || !file.type || file.type.indexOf("image/") !== 0) {
+      readNextPhoto();
+      return;
+    }
+
+    reader = new FileReader();
+
+    reader.onload = function (loadEvent) {
+      targetPhotos.push(loadEvent.target.result);
+      readNextPhoto();
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  readNextPhoto();
+}
+
+function renderPhotosSection() {
+  return '' +
+    '<section class="section section-tight">' +
+      renderSectionBar('Photos') +
+      '<div style="display: flex; flex-wrap: wrap; gap: 4mm; align-items: flex-start;">' +
+        renderPhotoGroup(customerPhotos, 'Customer photo') +
+        renderPhotoGroup(beforePhotos, 'Before photo') +
+        renderPhotoGroup(afterPhotos, 'After photo') +
+      '</div>' +
+    '</section>';
+}
+
+function renderPhotoGroup(photos, labelPrefix) {
+  var html = '';
+  var i;
+
+  for (i = 0; i < photos.length; i++) {
+    html += renderPhotoBox(photos[i], labelPrefix + ' ' + padNumber(i + 1));
+  }
+
+  return html;
+}
+
+function renderPhotoBox(photoDataUrl, label) {
+  return '' +
+    '<div style="width: 50mm;">' +
+      '<div style="width: 50mm; height: 50mm; border: 1px solid #d9d9d9; display: flex; align-items: center; justify-content: center; overflow: hidden; background: #fff;">' +
+        '<img src="' + photoDataUrl + '" alt="' + escapeHtml(label) + '" style="max-width: 100%; max-height: 100%; object-fit: contain; display: block;">' +
+      '</div>' +
+      '<div style="font-size: 10px; line-height: 1.2; margin-top: 1mm; text-align: center;">' + escapeHtml(label) + '</div>' +
+    '</div>';
 }
